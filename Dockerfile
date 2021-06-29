acb2ac7c12e43756b651260b0772b821313e8d1d
Christopher Buechner <christopher.buechner@maxar.com>
2021-06-29 13:27:46 -0500

    Added debug and central timezone

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
 # set base image (host OS)
 FROM python:3.10.0b3-alpine3.14
 
+# Set the appropriate timezone
+RUN apk add tzdata && \
+    cp /usr/share/zoneinfo/US/Central /etc/localtime && \
+    echo "US/Central" > /etc/timezone && \
+    apk del tzdata
+
 # install dependencies
 #RUN pip install -r requirements.txt
 
@@ -13,3 +19,4 @@ COPY . /stretchsms
 
 # Run the cron At minute 15 past every hour from 8-16.
 RUN echo '15       8-16       *       *       *       /stretchsms/startup.sh' >> /etc/crontabs/root
+
--- run.py
+++ run.py
@@ -8,10 +8,29 @@ EMAIL_PASS=os.environ.get("SMS_EMAIL_PASSWORD")
 TARGET_PHONE=os.environ.get("SMS_TARGET_PHONE")
 TARGET_PHONE_CARRIER=os.environ.get("SMS_TARGET_PHONE_CARRIER").lower()
 
+
+def parse_args():
+    my_parser = argparse.ArgumentParser(description='Argument Parsing for StretchSMS')
+
+    # Add the arguments
+    parser.add_argument('d', '--debug', dest='debug', action='store_true')
+
+    # Execute the parse_args() method
+    return my_parser.parse_args()
+
+input_path = args.Path
+
 if __name__ == "__main__":
-    #TODO - Cron job
-    #time = str(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
-    #text_message="\nIt's time to stretch! Another message will be coming at " + time + "."
-    text_message="\nIt's time to stretch!" # Leading newline strips extra text from SMS.
+    #Parse Args and get timestamp
+    args=parse_args()
+    time = str(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+
+    # Format message depending on runtype
+    if args.debug:
+        text_message="\nDebug message from StretchSMS at {time}" # Leading newline strips extra text from SMS.
+    else:
+        text_message="\nIt's time to stretch!" # Leading newline strips extra text from SMS.
+    
+    #text_message="\nIt's time to stretch! Another message will be coming at " + time + "."    
     SMS.send(EMAIL_ADDR, EMAIL_PASS, TARGET_PHONE, TARGET_PHONE_CARRIER, text_message)
     print("Complete")
