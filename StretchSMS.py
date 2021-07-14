cbcb0eae31e30bbe7b38e8251f1e3689128183ce
Christopher Buechner <christopher.buechner@maxar.com>
2021-07-14 17:56:42 -0500

    Added minor logging

--- StretchSMS.py
+++ StretchSMS.py
@@ -15,11 +15,15 @@ def send(email_acc, email_pw, target_phone, carrier, message):
     auth = (email_acc, email_pw)
 
     # Establish a secure session with gmail's outgoing SMTP server using your gmail account
+    print("Securing session..")
     server = smtplib.SMTP("smtp.gmail.com", 587)
+    print("Starting TLS..")
     server.starttls()
+    print("Authenticating..")
     server.login(auth[0], auth[1])
 
     # Send text message through SMS gateway of destination number
+    print("Sending Mail..")
     server.sendmail(auth[0], to_number, message)
 
 def validate_carrier(carrier):
